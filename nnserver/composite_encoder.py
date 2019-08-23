d43fb5a08ce915b5a9101fd48e01d3ca0552aa45
haukurb <haukzi@gmail.com>
2019-08-23 20:53:49 +0000

    Initial commit

---
+++ nnserver/composite_encoder.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env/python
+# coding=utf-8
+
+"""
+    Reynir: Natural language processing for Icelandic
+
+    Neural Network Parsing Encoder
+
+    Copyright (C) 2018 Miðeind ehf.
+
+       This program is free software: you can redistribute it and/or modify
+       it under the terms of the GNU General Public License as published by
+       the Free Software Foundation, either version 3 of the License, or
+       (at your option) any later version.
+       This program is distributed in the hope that it will be useful,
+       but WITHOUT ANY WARRANTY; without even the implied warranty of
+       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+       GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+
+
+    This module implements a composite subword encoder for tokens on
+    the output side of the text-to-parse-tree model, i.e. grammar
+    nonterminals, terminals and their variants.
+
+"""
+
+import os
+
+from tensor2tensor.data_generators import text_encoder
+from nnserver.parsing_subtokens import ParsingSubtokens
+
+from nnserver.nnserver import _PARSING_VOCAB
+
+_CASE_TOKS = set(["nf", "þf", "þgf", "ef"])
+
+UNK = "<UNK>"
+EOS_ID = text_encoder.EOS_ID
+
+
+class CompositeTokenEncoder(text_encoder.TextEncoder):
+    """Build vocabulary from composite token vocabulary
+
+    Read composite vocabulary and extract subtokens according to simple
+    right recursive rule (regular) with a couple of exceptions
+
+    Behaves otherwise similarly to the Tensor2Tensor subword encoders
+    """
+
+    def __init__(self, filename=None, reorder=True, version=1):
+        if filename is None:
+            self.filename = _PARSING_VOCAB
+        else:
+            self.filename = filename
+        self.version = version
+
+        tokens = ParsingSubtokens(self.filename, version=self.version)
+
+        self._num_reserved_ids = len(text_encoder.RESERVED_TOKENS)
+        self._reorder = reorder
+        self._preprocess_word = tokens.preprocess_word
+
+        self._tok_id_to_tok_str = tokens._tok_id_to_tok_str
+        self._ftok_to_tok_id = tokens._ftok_to_tok_id
+        self._htok_to_tok_id = tokens._htok_to_tok_id
+        self._ttok_to_tok_id = tokens._ttok_to_tok_id
+        self.oov_id = tokens.oov_id
+
+    @property
+    def num_reserved_ids(self):
+        return self._num_reserved_ids
+
+    def _token_to_subtoken_ids(self, word):
+        word = self._preprocess_word(word)
+        if word in self._ftok_to_tok_id:
+            return [self._ftok_to_tok_id[word]]
+        if "_" not in word:
+            return [self.oov_id]
+        toks = word.split("_")
+        head, t1 = toks[:2]
+        tail = toks[2:]
+
+        tail_start = 0
+        if t1 in {"0", "1", "2", "subj"}:
+            head = head + "_" + t1
+            tail_start += 0 if t1 == "subj" else int(t1)
+        else:
+            tmp = [t1]
+            tmp.extend(tail)
+            tail = tmp
+
+        if head not in self._htok_to_tok_id or not all(
+            t in self._ttok_to_tok_id for t in tail
+        ):
+            return [self.oov_id]
+
+        ids = [self._htok_to_tok_id[head]]
+        tail = [self._ttok_to_tok_id[t] for t in tail]
+        if self._reorder:
+            tail[tail_start:] = sorted(tail[tail_start:])
+        ids.extend(tail)
+        return ids
+
+    def token_to_subtokens(self, word):
+        remove_slash = lambda t: t if t[0] != "_" else t[1:]
+        subtokens = [
+            remove_slash(self._tok_id_to_tok_str[t])
+            for t in self._token_to_subtoken_ids(word)
+        ]
+        return subtokens
+
+    def _tokens_to_subtoken_ids(self, words):
+        numres = self._num_reserved_ids
+        result = []
+        for word in words:
+            ids = [(i + numres) for i in self._token_to_subtoken_ids(word)]
+            result.extend(ids)
+        return result
+
+    def encode(self, string):
+        result = self._tokens_to_subtoken_ids(list(string.split(" ")))
+        return result
+
+    def decode(self, ids):
+        numres = self._num_reserved_ids
+        RES_TOKENS = text_encoder.RESERVED_TOKENS
+        result = []
+        for idx in range(len(ids)):
+            tid = ids[idx]
+            if 0 <= tid < numres:
+                result.append(RES_TOKENS[tid])
+                continue
+
+            tid -= numres
+            result.append(self._tok_id_to_tok_str[tid])
+            is_full_tok = tid in self._ftok_to_tok_id.values()
+            end_of_tail = tid in self._ttok_to_tok_id.values()
+            is_oov = tid == self.oov_id
+            end_of_tail &= (
+                idx + 1 < len(ids)
+                and (ids[idx + 1] - numres) not in self._ttok_to_tok_id.values()
+            )
+            if is_full_tok or end_of_tail or is_oov:
+                result.append(" ")
+        return "".join(result).strip()
+
+    def decode_list(self, ids):
+        numres = self._num_reserved_ids
+        RES_TOKENS = text_encoder.RESERVED_TOKENS
+        result = []
+        for idx in range(len(ids)):
+            tid = ids[idx]
+            if 0 <= tid < numres:
+                result.append(RES_TOKENS[tid])
+                continue
+            tid = tid - numres
+            tok = self._tok_id_to_tok_str[tid]
+            tok = tok + "_" if tid in self._htok_to_tok_id else tok
+            result.append(tok)
+        return result
+
+    @property
+    def vocab_size(self):
+        return (
+            self._num_reserved_ids
+            + len(self._ftok_to_tok_id)
+            + len(self._htok_to_tok_id)
+            + len(self._ttok_to_tok_id)
+        )
+
+
+def test_roundtrip():
+    sample = "P S-MAIN IP NP-SUBJ pfn_et_nf_p3 /NP-SUBJ /IP /S-MAIN /P"
+    default_encoder = CompositeTokenEncoder()
+    subtoken_ids = default_encoder.encode(sample)
+    decoded_sample = default_encoder.decode(subtoken_ids)
+    assert sample == decoded_sample, "Encoding roundtrip does not match"
+
+
+if __name__ == "__main__":
+    test_roundtrip()
---
+++ nnserver/nnserver.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python3
+"""
+    Reynir: Natural language processing for Icelandic
+
+    Neural Network Query Client
+
+    Copyright (C) 2018 Miðeind ehf.
+
+       This program is free software: you can redistribute it and/or modify
+       it under the terms of the GNU General Public License as published by
+       the Free Software Foundation, either version 3 of the License, or
+       (at your option) any later version.
+       This program is distributed in the hope that it will be useful,
+       but WITHOUT ANY WARRANTY; without even the implied warranty of
+       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+       GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+
+
+    This module implements a server that provides an Icelandic text interface to
+    a Tensorflow model server running a text-to-parse-tree neural network.
+
+    Example usage:
+    python nnserver.py -lh 0.0.0.0 -lp 8080 -mh localhost -mp 9001
+
+    To curl a running nnserver try:
+    curl --header "Content-Type: application/json" \
+        --request POST \
+        http://localhost:8080/translate.api \
+        --data '{"pgs":["Hvernig komstu þangað?"],"signature_name":"serving_default"}'
+
+    To test a running model server directly, try:
+    curl --header "Content-Type: application/json" \
+        --request POST \
+        http://localhost:8080/v1/models/transformer:predict \
+        --data '{"instances":[{"input":{"b64":"CiAKHgoGaW5wdXRzEhQaEgoQpweFF0oOBsI+EclIBP4cAQ=="}}],"signature_name":"serving_default"}'
+
+    To test a running model server directly and receive gRPC error feedback (more informative than RESTful):
+    cd tf/lib/python3.5/site-packages
+    python tensor2tensor/serving/query.py \
+        --server 'localhost:8081' \
+        --servable_name transformer \
+        --data_dir ~/t2t_data \
+        --t2t_usr_dir ~/t2t_usr \
+        --problem translate_enis16k_rev \
+        --inputs_once "Kominn."
+"""
+
+import requests
+import base64
+import json
+import os
+import pkg_resources
+
+from tensor2tensor.data_generators import text_encoder
+from tensorflow.core.example import feature_pb2
+from tensorflow.core.example import example_pb2
+
+from flask import Flask, jsonify, request
+
+
+EOS_ID = text_encoder.EOS_ID
+PAD_ID = text_encoder.PAD_ID
+
+_RESOURCES = pkg_resources.resource_filename(__package__, "resources")
+_ENIS_VOCAB = os.path.join(_RESOURCES, "vocab.translate_enis16k.16384.subwords")
+_PARSING_VOCAB = os.path.join(_RESOURCES, "parsing_tokens_180729.txt")
+
+from nnserver.composite_encoder import CompositeTokenEncoder
+
+
+app = Flask(__name__)
+
+
+class NnServer:
+    """ Client that mimics the HTTP RESTful interface of
+        a tensorflow model server, but accepts plain text. """
+
+    _tfms_version = "v1"
+    _model_name = "transformer"
+    _verb = "predict"
+    src_enc = None
+    tgt_enc = None
+
+    @classmethod
+    def request(cls, pgs):
+        """ Send serialized request to remote model server """
+        url = "http://{host}:{port}/{version}/models/{model}:{verb}".format(
+            port=app.config.get("out_port"),
+            host=app.config.get("out_host"),
+            version=cls._tfms_version,
+            model=cls._model_name,
+            verb=cls._verb,
+        )
+        instances = [cls.serialize_to_instance(sent) for sent in pgs]
+        payload = {"signature_name": "serving_default", "instances": instances}
+        payload = json.dumps(payload)
+        headers = {"content-type": "application/json"}
+
+        resp = requests.post(url, data=payload, headers=headers)
+        resp.raise_for_status()
+
+        obj = json.loads(resp.text)
+        predictions = obj["predictions"]
+        results = [
+            cls.process_response_instance(inst, sent)
+            for (inst, sent) in zip(predictions, pgs)
+        ]
+        obj["predictions"] = results
+        return obj
+
+    @classmethod
+    def process_response_instance(cls, instance, sent, src_enc=None, tgt_enc=None):
+        """  Process the numerical output from the model server for one sentence """
+        src_enc = src_enc or cls.src_enc
+        tgt_enc = tgt_enc or cls.tgt_enc
+
+        scores = instance["scores"]
+        output_ids = instance["outputs"]
+
+        app.logger.debug("scores: " + str(scores))
+        app.logger.debug("output_ids: " + str(output_ids))
+
+        # Strip padding and eos token
+        length = len(output_ids)
+        pad_start = output_ids.index(PAD_ID) if PAD_ID in output_ids else length
+        eos_start = output_ids.index(EOS_ID) if EOS_ID in output_ids else length
+        sent_end = min(pad_start, eos_start)
+        output_toks = cls.tgt_enc.decode(output_ids[:sent_end])
+
+        app.logger.debug(
+            "tokenized and depadded: "
+            + str(cls.tgt_enc.decode_list(output_ids[:sent_end]))
+        )
+        app.logger.info(output_toks)
+
+        instance["outputs"] = output_toks
+        return instance
+
+    @classmethod
+    def serialize_to_instance(cls, sent, src_enc=None, tgt_enc=None):
+        """ Encodes a single sentence into the format expected by the RESTful interface
+        of tensorflow_model_server running an exported tensor2tensor transformer translation model """
+        # Add end of sentence token
+        src_enc = src_enc or cls.src_enc
+        tgt_enc = tgt_enc or cls.tgt_enc
+
+        input_ids = cls.src_enc.encode(sent)
+        app.logger.info("received: " + sent)
+        app.logger.debug("tokenized: " + str(cls.src_enc.decode_list(input_ids)))
+        app.logger.debug("input_ids: " + str(input_ids))
+        input_ids.append(EOS_ID)
+
+        int64_list = feature_pb2.Int64List(value=input_ids)
+        feature = feature_pb2.Feature(int64_list=int64_list)
+        feature_map = {"inputs": feature}
+        features = feature_pb2.Features(feature=feature_map)
+        example = example_pb2.Example(features=features)
+
+        b64_example = base64.b64encode(example.SerializeToString()).decode()
+        return {"input": {"b64": b64_example}}
+
+
+class ParsingServer(NnServer):
+    """ Client that accepts plain text Icelandic
+        and returns a flattened parse tree according
+        to the Reynir schema """
+
+    src_enc = text_encoder.SubwordTextEncoder(_ENIS_VOCAB)
+    # tgt_enc = CompositeTokenEncoder(_PARSING_VOCAB_PATH, version=1)
+    tgt_enc = CompositeTokenEncoder(_PARSING_VOCAB, version=1)
+    _model_name = "parse"
+
+
+class TranslateServer(NnServer):
+    """ Client that accepts plain text Icelandic
+        and returns an English translation of the text """
+
+    src_enc = text_encoder.SubwordTextEncoder(_ENIS_VOCAB)
+    tgt_enc = src_enc
+    # _model_name = "translate"
+    _model_name = "translate_v2"
+
+
+@app.route("/parse.api", methods=["POST"])
+def parse_api():
+    try:
+        req_body = request.data.decode("utf-8")
+        obj = json.loads(req_body)
+        # TODO: validate form?
+        pgs = obj["pgs"]
+        model_response = ParsingServer.request(pgs)
+        resp = jsonify(model_response)
+    except Exception as error:
+        resp = jsonify(valid=False, reason="Invalid request")
+        app.logger.exception(error)
+    resp.headers["Content-Type"] = "application/json; charset=utf-8"
+    return resp
+
+
+@app.route("/translate.api", methods=["POST"])
+def translate_api():
+    try:
+        req_body = request.data.decode("utf-8")
+        obj = json.loads(req_body)
+        pgs = obj["pgs"]
+        model_response = TranslateServer.request(pgs)
+        resp = jsonify(model_response)
+    except Exception as error:
+        resp = jsonify(valid=False, reason="Invalid request")
+        app.logger.exception(error)
+    resp.headers["Content-Type"] = "application/json; charset=utf-8"
+    return resp
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description=(
+            "Middleware server that provides a textual interface to tensorflow model server"
+        )
+    )
+    parser.add_argument(
+        "-lh",
+        "--listen_host",
+        dest="IN_HOST",
+        default="0.0.0.0",
+        required=False,
+        type=str,
+        help="Hostname to listen on",
+    )
+    parser.add_argument(
+        "-lp",
+        "--listen_port",
+        dest="IN_PORT",
+        default="8080",
+        required=False,
+        type=str,
+        help="Port to listen on",
+    )
+    parser.add_argument(
+        "-mh",
+        "--model_host",
+        dest="OUT_HOST",
+        default="localhost",
+        required=False,
+        type=str,
+        help="Hostname of model server",
+    )
+    parser.add_argument(
+        "-mp",
+        "--model_port",
+        dest="OUT_PORT",
+        default="9000",
+        required=False,
+        type=str,
+        help="Port of model server",
+    )
+    parser.add_argument(
+        "--debug",
+        dest="DEBUG",
+        default=False,
+        action="store_true",
+        required=False,
+        help="Emit debug information messages",
+    )
+    parser.add_argument(
+        "--only",
+        dest="ONLY",
+        default=False,
+        required=False,
+        type=str,
+        choices=["parse", "translate"],
+        help="Only use one model (otherwise both).",
+    )
+    args = parser.parse_args()
+    app.config["out_host"] = args.OUT_HOST
+    app.config["out_port"] = args.OUT_PORT
+    app.run(threaded=True, debug=args.DEBUG, host=args.IN_HOST, port=args.IN_PORT)
